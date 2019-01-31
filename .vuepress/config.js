f8b17a2eb9aab6d7db780196963f13e165e8391a
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2019-01-31 11:42:20 +0100

    Add initial config and example chapter

---
+++ .vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: 'Vue cookbook', // Page title
+  description: 'Live e-book with recipes for making cool stuff with Vue',
+  theme: 'live-ebook', // Important! This is the name of this theme
+  base: '/', // Base URL, leave as is if you don't serve the book from a folder
+  head: [
+    ['link', { rel: 'icon', href: '/images/favicon.ico' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+  ],
+  themeConfig: {
+    bookTitle: 'Vue cookbook', // Book title
+    baseUrl: 'https://github.com/mateuszRybczonek', // Used for sharing in social media and for meta tags
+    exitUrl: 'https://github.com/mateuszRybczonek',
+    company: {
+      name: 'Mateusz Rybczonek',
+      url: 'https://github.com/mateuszRybczonek',
+      logo: '/images/netguru.svg'
+    }
+  },
+
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  }
+}
