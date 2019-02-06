4de9f0f40d9e22ce0d4f7d96c87c880e19f5f0c1
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2019-02-06 15:43:33 +0100

    VC-5 Add BaseBadge component

---
+++ .vuepress/components/BaseBadge.vue
@@ -0,0 +1,27 @@
+<template>
+  <span class="base-badge">
+    {{ label }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$c-primary-accent--dark: #dd8346;
+
+.base-badge {
+  padding: 3px 12px;
+  font-size: 12px;
+  border-radius: 20px;
+  background-color:$c-primary-accent--dark;
+}
+</style>
